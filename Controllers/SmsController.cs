264755ee49975f194a37df5dd26327fbd6d74258
Fatima Mughal <fatimamughal651@gmail.com>
2024-02-18 23:14:45 +0500

    twillio

--- Controllers/SmsController.cs
+++ Controllers/SmsController.cs
@@ -1,45 +1,45 @@
 ﻿using FYPM.Models;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Configuration;
 using System.Net;
 using System.Web.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace FYPM.Controllers
 {
     public class SmsController : Controller
     {
-        private readonly ILogger<SmsController> _logger;
-        private readonly ISmsService _smsService;
-
-        public SmsController(ISmsService smsService, ILogger<SmsController> logger)
+        
+        public ActionResult Sms()
         {
-            _smsService = smsService;
-            _logger = logger;
-        }
+            string accountSid = ConfigurationManager.AppSettings["AC66d0c98f4daedd97410a92307386c62b"];
+            string authToken = ConfigurationManager.AppSettings["7a5063806b1433de80234d635e7e25bd"];
+            string TwilioPhoneNumber = ConfigurationManager.AppSettings["+16815324812"];
+         
+        TwilioClient.Init(accountSid, authToken);
 
-        // Action for sending an SMS
-        [HttpPost]
-        public ActionResult SendSms(SendSmsRequestDTO model)
-        {
             try
             {
-                var response = _smsService.SendSms(model);
-                if (response.Success)
-                {
-                    return Json(response);
-                }
-                else
-                {
-                    // Return a status code 400 (Bad Request) with the error message
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest, response.Message);
-                }
+                // Send an SMS message
+                var message = MessageResource.Create(
+                    body: "Test message from Twilio!",
+                    from: new Twilio.Types.PhoneNumber(TwilioPhoneNumber),
+                    to: new Twilio.Types.PhoneNumber("+14243533831")
+                );
+
+                // Optionally, you can handle the response here, log it, etc.
+                // For example:
+                ViewBag.MessageSid = message.Sid;
+                ViewBag.MessageStatus = message.Status;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error sending SMS");
-                // Return a status code 500 (Internal Server Error)
-                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "Internal server error");
+                // Handle any exceptions that might occur during message sending
+                ViewBag.ErrorMessage = ex.Message;
             }
+            return View();
         }
     }
 }
