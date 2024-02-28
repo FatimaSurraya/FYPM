2c03756b8339a5b9cc1a5ecf50b0094749092e09
Fatima Mughal <fatimamughal651@gmail.com>
2024-02-29 00:43:28 +0500

    tw

--- Controllers/SmsController.cs
+++ Controllers/SmsController.cs
@@ -11,39 +11,22 @@ using Twilio.Types;
 namespace FYPM.Controllers
 {
     public class SmsController : Controller
-    {  public ActionResult GetSms()
+    {   public readonly TwilioSmsService smsService;
+        public SmsController()
         {
-            return View();
+            smsService = new TwilioSmsService(
+                ConfigurationManager.AppSettings["TwilioAccountSid"],
+                ConfigurationManager.AppSettings["TwilioAuthToken"]
+            );
         }
-        
-        public ActionResult Create()
+        [HttpPost]
+        public ActionResult SendSms(string to, string message)
         {
-            string accountSid = "AC66d0c98f4daedd97410a92307386c62b";
-            string authToken = "ebea45833792178bf2ae72c7f9aaed9e";
-            string phone = "+16815324812";
-
-            TwilioClient.Init(accountSid, authToken);
-
-            try
-            {
-                // Send an SMS message
-                var message = MessageResource.Create(
-                    body: "Test message from Twilio!",
-                    from: new PhoneNumber(phone),
-                    to: new PhoneNumber("+14243533831") // Replace with your desired recipient phone number
-                );
-
-                // If the message was sent successfully
-                ViewData["Success"] = message.Sid;
-            }
-            catch (Exception ex)
-            {
-                // If there was an error sending the message
-                ViewData["Error"] = "Error sending SMS: " + ex.Message;
-            }
-
-            return View();
+           smsService.SendMessage(to, message);
+            return Content("SMS sent successfully!");
         }
+       
+       
     }
 }
    
--- TwilioSmsService.cs
+++ TwilioSmsService.cs
@@ -1,6 +1,7 @@
 ﻿using FYPM.Models;
 using FYPM.Models.ViewModel;
 using Microsoft.Extensions.Logging;
+using System;
 using Twilio;
 using Twilio.Exceptions;
 using Twilio.Rest.Api.V2010.Account;
@@ -13,14 +14,22 @@ namespace FYPM
     public class TwilioSmsService : ISmsService
     {
         private readonly string accountSid = "AC66d0c98f4daedd97410a92307386c62b";
-        private readonly string authToken = "[7a5063806b1433de80234d635e7e25bd]";
+        private readonly string authToken = "[ebea45833792178bf2ae72c7f9aaed9e]";
         private readonly string phNumber = "+16815324812";
         private readonly string serviceSid = "[AC66d0c98f4daedd97410a92307386c62b]";
         private readonly ILogger _logger;
+        private string v1;
+        private string v2;
 
-        public TwilioSmsService(ILogger<MessageViewModel> logger)
+        public TwilioSmsService(string v, ILogger<MessageViewModel> logger)
         {
             _logger = logger;
+        }
+
+        public TwilioSmsService(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
         }
 
         public ResponseDTO<bool> SendSms(SendSmsRequestDTO model)
@@ -37,7 +46,7 @@ namespace FYPM
                 var message = MessageResource.Create(
                     body: newMessage,
                     from: new PhoneNumber(phNumber),
-                    to: new PhoneNumber(number)
+                    to: new PhoneNumber("+14243533831")
                 );
 
                 // Use Responses.OK for success
@@ -52,5 +61,9 @@ namespace FYPM
             }
         }
 
+        internal void SendMessage(string to, string message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
